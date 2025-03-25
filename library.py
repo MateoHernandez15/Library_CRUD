fda1244cdd07c1f584e1a151a27d89d6981ce27d
Mateo <mateoandresher@gmail.com>
2025-03-24 20:44:07 -0500

    second US

--- library.py
+++ library.py
@@ -18,4 +18,24 @@ def createUser():
         "contraseña": user_password
     }
     print(f"Usuario {user_name} registrado correctamente.\n")   
-    
+
+#US2/ LOGIN AND VERIFICATION TO PASSWORD
+def login():
+    id = input("Ingrese su documento: ")
+    if id not in users:
+        print("Este usuario no esta registrado. Por favor, cree una cuenta.\n")
+        return None
+    password = input("Ingrese su contraseña: ")
+    if users[id]["contraseña"] == password:
+        print(f"Bienvenido {users[id]['nombre']}!\n")
+        return id
+    else:
+        print("Contraseña incorrecta.\n")
+        return None
+def showUsers():
+    if not users:
+        print("No hay usuarios registrados.\n")
+        return
+    for id, user in users.items():
+        print(f"Documento: {id}, Nombre: {user['nombre']} {user['apellido']}, Teléfono: {user['telefono']}, Email: {user['email']}")
+    print()
